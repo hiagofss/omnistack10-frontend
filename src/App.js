19babea681d935642aa26f9d63e0701abec2d9e5
Hiago Souza <hiagofss98@gmail.com>
2020-01-15 21:52:12 -0300

    Styled Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,130 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="username">Username do GitHub</label>
+            <input
+              type="text"
+              name="github_username"
+              id="github_username"
+              required
+            />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input type="text" name="techs" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="text" name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="text" name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">SALVAR</button>{' '}
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/29542456?s=460&v=4"
+                alt="Hiago Souza"
+              />
+              <div className="user-info">
+                <strong>Hiago Souza</strong>
+                <span>NodeJS, React e ReacNative</span>
+              </div>
+            </header>
+            <p>Software Developer apaixonado pelo que faço!</p>
+            <a href="https://github.com/hiagofss">Acessar Perfil do GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/29542456?s=460&v=4"
+                alt="Hiago Souza"
+              />
+              <div className="user-info">
+                <strong>Hiago Souza</strong>
+                <span>NodeJS, React e ReacNative</span>
+              </div>
+            </header>
+            <p>Software Developer apaixonado pelo que faço!</p>
+            <a href="https://github.com/hiagofss">Acessar Perfil do GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/29542456?s=460&v=4"
+                alt="Hiago Souza"
+              />
+              <div className="user-info">
+                <strong>Hiago Souza</strong>
+                <span>NodeJS, React e ReacNative</span>
+              </div>
+            </header>
+            <p>Software Developer apaixonado pelo que faço!</p>
+            <a href="https://github.com/hiagofss">Acessar Perfil do GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/29542456?s=460&v=4"
+                alt="Hiago Souza"
+              />
+              <div className="user-info">
+                <strong>Hiago Souza</strong>
+                <span>NodeJS, React e ReacNative</span>
+              </div>
+            </header>
+            <p>Software Developer apaixonado pelo que faço!</p>
+            <a href="https://github.com/hiagofss">Acessar Perfil do GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/29542456?s=460&v=4"
+                alt="Hiago Souza"
+              />
+              <div className="user-info">
+                <strong>Hiago Souza</strong>
+                <span>NodeJS, React e ReacNative</span>
+              </div>
+            </header>
+            <p>Software Developer apaixonado pelo que faço!</p>
+            <a href="https://github.com/hiagofss">Acessar Perfil do GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/29542456?s=460&v=4"
+                alt="Hiago Souza"
+              />
+              <div className="user-info">
+                <strong>Hiago Souza</strong>
+                <span>NodeJS, React e ReacNative</span>
+              </div>
+            </header>
+            <p>Software Developer apaixonado pelo que faço!</p>
+            <a href="https://github.com/hiagofss">Acessar Perfil do GitHub</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
